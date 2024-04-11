buildfarm/buildfarm

Supply a basic retrier to remote cas writes
Worker loss can signal cascading failure and shutdown for execute-only
peers. Ensure that a ReportResultStage seeing an SRE does not close the
stage, and that there are basic retries for remote uploads.

src/main/java/build/buildfarm/worker/shard/Worker.java
@@ -39,6 +39,8 @@ import build.buildfarm.common.InputStreamFactory;
 import build.buildfarm.common.config.BuildfarmConfigs;
 import build.buildfarm.common.config.Cas;
 import build.buildfarm.common.config.GrpcMetrics;
+import build.buildfarm.common.grpc.Retrier;
+import build.buildfarm.common.grpc.Retrier.Backoff;
 import build.buildfarm.common.services.ByteStreamService;
 import build.buildfarm.common.services.ContentAddressableStorageService;
 import build.buildfarm.instance.Instance;
@@ -577,7 +579,8 @@ public class Worker {
     // Create the appropriate writer for the context
     CasWriter writer;
     if (!configs.getWorker().getCapabilities().isCas()) {
-      writer = new RemoteCasWriter(backplane.getStorageWorkers(), workerStubs);
+      Retrier retrier = new Retrier(Backoff.sequential(5), Retrier.DEFAULT_IS_RETRIABLE);
+      writer = new RemoteCasWriter(backplane.getStorageWorkers(), workerStubs, retrier);
     } else {
       writer = new LocalCasWriter(execFileSystem);
     }

bazel-ios/bazel-buildfarm

src/main/java/build/buildfarm/worker/shard/Worker.java
@@ -40,6 +40,8 @@ import build.buildfarm.common.InputStreamFactory;
 import build.buildfarm.common.config.BuildfarmConfigs;
 import build.buildfarm.common.config.Cas;
 import build.buildfarm.common.config.GrpcMetrics;
+import build.buildfarm.common.grpc.Retrier;
+import build.buildfarm.common.grpc.Retrier.Backoff;
 import build.buildfarm.common.services.ByteStreamService;
 import build.buildfarm.common.services.ContentAddressableStorageService;
 import build.buildfarm.instance.Instance;
@@ -606,7 +608,8 @@ public class Worker {
     // Create the appropriate writer for the context
     CasWriter writer;
     if (!configs.getWorker().getCapabilities().isCas()) {
-      writer = new RemoteCasWriter(backplane.getWorkers(), workerStubs);
+      Retrier retrier = new Retrier(Backoff.sequential(5), Retrier.DEFAULT_IS_RETRIABLE);
+      writer = new RemoteCasWriter(backplane.getWorkers(), workerStubs, retrier);
     } else {
       writer = new LocalCasWriter(execFileSystem);
     }