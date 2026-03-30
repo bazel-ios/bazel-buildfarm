buildfarm/buildfarm

Prefer Action's Platform to Command
REAPI v2.2 indicates that the Platform contained within Actions should
be respected over the field in Command. Prefer this and use only
QueueEntry Platform, populated from the server, in the worker.

src/main/java/build/buildfarm/worker/Executor.java
@@ -167,7 +167,7 @@ class Executor {
     if (limits.useExecutionPolicies) {
       policies =
           ExecutionPolicies.forPlatform(
-              executionContext.command.getPlatform(), workerContext::getExecutionPolicies);
+              executionContext.queueEntry.getPlatform(), workerContext::getExecutionPolicies);
     }
     // since pools mandate injection of resources, each pool claim must require a policy
     // could probably do this with just the properties and the pool resource sets, but this is
@@ -297,7 +297,7 @@ class Executor {
     // similar to the policy selection here
     Map<String, Interpolator> interpolations =
         createInterpolations(
-            executionContext.claim, executionContext.command.getPlatform().getPropertiesList());
+            executionContext.claim, executionContext.queueEntry.getPlatform().getPropertiesList());
 
     ImmutableList.Builder<String> arguments = ImmutableList.builder();
 

bazel-ios/bazel-buildfarm

src/main/java/build/buildfarm/worker/Executor.java
@@ -106,7 +106,7 @@ class Executor {
     if (limits.useExecutionPolicies) {
       policies =
           ExecutionPolicies.forPlatform(
-              operationContext.command.getPlatform(), workerContext::getExecutionPolicies);
+              operationContext.queueEntry.getPlatform(), workerContext::getExecutionPolicies);
     }
 
     Operation operation =